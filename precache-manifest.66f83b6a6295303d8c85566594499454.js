6f715f5821b499452658ed2efd949fc1b00d3e76
Craig Iturbe <citurbe@gmail.com>
2019-06-01 16:34:03 -0400

    Updates

---
+++ precache-manifest.66f83b6a6295303d8c85566594499454.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f97d61a2501fd8b630b00a56071776a",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "1ce812dc459af86a48b5",
+    "url": "/weather/static/css/main.f87a80f6.chunk.css"
+  },
+  {
+    "revision": "74426a666566c43e525b",
+    "url": "/weather/static/js/2.dcfa3d09.chunk.js"
+  },
+  {
+    "revision": "1ce812dc459af86a48b5",
+    "url": "/weather/static/js/main.e956c923.chunk.js"
+  },
+  {
+    "revision": "3cb3cde5c978189545b4",
+    "url": "/weather/static/js/runtime~main.6cff1e35.js"
+  }
+]);
